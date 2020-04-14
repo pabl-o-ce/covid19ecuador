7f030221b05d01937ebc0badb2428af2840182ae
Pablo Carrera Estrada <pablocarrera3@gmail.com>
2020-04-14 13:08:06 -0500

    Updates

---
+++ precache-manifest.5e2e9c5f57f3536892b0a79205a9b5bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897e226ba89130a0faf0c423f0a22eb2",
+    "url": "/covid19ecuador/index.html"
+  },
+  {
+    "revision": "9b5974a3d4d01684e2ae",
+    "url": "/covid19ecuador/static/css/main.d03def3b.chunk.css"
+  },
+  {
+    "revision": "d8bc8bdbe790fb6d05aa",
+    "url": "/covid19ecuador/static/js/2.4c8f84ac.chunk.js"
+  },
+  {
+    "revision": "c74c4fc83bebf8f8e93a396c2e1e49fd",
+    "url": "/covid19ecuador/static/js/2.4c8f84ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5974a3d4d01684e2ae",
+    "url": "/covid19ecuador/static/js/main.07d00258.chunk.js"
+  },
+  {
+    "revision": "0efd882f52ec2425a3b3",
+    "url": "/covid19ecuador/static/js/runtime-main.41455037.js"
+  },
+  {
+    "revision": "df3a0094b63fc45c1eb110b4d26c6323",
+    "url": "/covid19ecuador/static/media/logo.df3a0094.svg"
+  }
+]);
